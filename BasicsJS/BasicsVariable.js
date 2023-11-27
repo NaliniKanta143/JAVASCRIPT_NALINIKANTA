cd0414e4859b617f067fde82a5236e08d184ee23
Nalinikanta Sahoo <81829441+NaliniKanta143@users.noreply.github.com>
2023-11-27 08:06:25 +0000

    container config done

---
+++ BasicsJS/BasicsVariable.js
@@ -0,0 +1,12 @@
+const accountId =1313865
+let accountemail= "nsdfhiubfu@gmial.com"
+var accountpasword = "12345"
+accountCity = "ANGUL"
+
+//console.log(accountId);
+/*
+
+Don't use var in js.
+
+*/
+console.table([accountId,accountemail,accountpasword,accountCity]);
