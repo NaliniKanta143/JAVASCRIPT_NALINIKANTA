502bcc22973e6cce40e98eb5f47e930131ab7dad
Nalinikanta Sahoo <81829441+NaliniKanta143@users.noreply.github.com>
2023-11-27 08:30:34 +0000

    datatype

---
+++ BasicsJS/BasicsDatatype.js
@@ -0,0 +1,12 @@
+"use strict";//Treat all JS as near version
+
+let name = "Nalinikanta Sahoo"
+let age = 26
+let profession = "Develpoer"
+let lastlogin = false
+
+console.table([name,age,profession,lastlogin])
+
+console.log(typeof name)
+console.log(typeof lastlogin)
+console.log(typeof age)
