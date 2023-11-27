a2afb5f53a5239b61b56d043a8ab3dd795b8c499
Nalinikanta Sahoo <81829441+NaliniKanta143@users.noreply.github.com>
2023-11-27 14:04:33 +0000

    conversion

--- BasicsJS/BasicsDatatype.js
+++ BasicsJS/BasicsDatatype.js
@@ -1,12 +1,20 @@
 "use strict";//Treat all JS as near version
 
-let name = "Nalinikanta Sahoo"
-let age = 26
-let profession = "Develpoer"
-let lastlogin = false
+// let name = "Nalinikanta Sahoo"
+// let age = 26
+// let profession = "Develpoer"
+// let lastlogin = false
 
-console.table([name,age,profession,lastlogin])
+// console.table([name,age,profession,lastlogin])
 
-console.log(typeof name)
-console.log(typeof lastlogin)
-console.log(typeof age)
+// console.log(typeof name)
+// console.log(typeof lastlogin)
+// console.log(typeof age)
+
+// let score = "33"
+// let valueinnumber = Number(score)
+// console.log(typeof valueinnumber);
+
+let lastlogin1 = ""
+let bolaencheak = Boolean(lastlogin1)
+console.log(bolaencheak);
