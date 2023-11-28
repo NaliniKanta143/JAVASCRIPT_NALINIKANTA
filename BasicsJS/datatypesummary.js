12118e8c5648fe0370a44bee79b523c47343c657
Nalinikanta Sahoo <81829441+NaliniKanta143@users.noreply.github.com>
2023-11-28 19:50:10 +0000

    commit

---
+++ BasicsJS/datatypesummary.js
@@ -0,0 +1,23 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const bigNumber = 3456543576654356754n
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+console.log(myObj);
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+//++Memory allocation++
+
+//Stack Memory(Primitive) Heap(Non-Primitive)
+
