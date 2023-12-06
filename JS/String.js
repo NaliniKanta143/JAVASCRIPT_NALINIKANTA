93d4004c1e6c4d253d5ee633bda2d84dc11892a4
Nalinikanta Sahoo <81829441+NaliniKanta143@users.noreply.github.com>
2023-12-06 19:58:22 +0000

    String

---
+++ JS/String.js
@@ -0,0 +1,24 @@
+const name = "LIKI"
+const repocount = 50
+
+// console.log(name + repocount+"value");
+// console.log(`Hello my name is ${name} and my repocount is ${repocount} ThankYou!!!`);
+
+const gamename = new String('Karnel')
+// console.log(gamename[0]);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(0));
+// console.log(gamename.indexOf('r'));
+// console.log(gamename.substring(1,5));
+
+const anotherstring = gamename.slice(-1,4)
+console.log(anotherstring);
+
+const newstring1 = '    Nalinikanta Sahoo   '
+console.log(newstring1.trim());
+console.log(newstring1.replace('Nalinikanta','Liki'));
+console.log(newstring1.includes('Sahoo'));
+console.log(newstring1.split('Nalinikanta',2))
+
+
