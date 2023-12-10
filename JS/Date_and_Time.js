7f0617c54d8c15f5e01a20ba1ed96c971e90a6e1
Nalinikanta Sahoo <81829441+NaliniKanta143@users.noreply.github.com>
2023-12-10 19:36:51 +0000

    date/time

---
+++ JS/Date_and_Time.js
@@ -0,0 +1,8 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+let myCreatedDate = new Date("01-14-2023")
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
